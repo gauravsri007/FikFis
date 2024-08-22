279c47657cef6070743f3a30795e5de75105f766
gauravsri007 <kumar.gaurav.sri99@gmail.com>
2024-08-22 10:44:10 +0530

    Assests added for account screen.

--- FikFis/Screens/Account/AccountV.swift
+++ FikFis/Screens/Account/AccountV.swift
@@ -7,9 +7,68 @@
 
 import SwiftUI
 
-struct AccountV: View {
+struct HeaderTab: View {
+    let collection : Card
+    @State var size: CGFloat = 20
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Spacer()
+            Image("\(collection.imageUrl)")
+                .resizable()
+                .cornerRadius(12)
+                .frame(width: size, height: size)
+                .aspectRatio(contentMode: .fit)
+            Spacer()
+            Text(collection.title)
+                .font(.custom_font(.regular,size: 12))
+                .multilineTextAlignment(.center)
+                .lineLimit(2)
+                .frame(width: 100, height: 40, alignment: .top)
+                .background(Color.red)
+
+            Spacer()
+
+        }
+        .frame(width: (UIScreen.screenWidth / 2) - 30, height: 50, alignment: .top)
+        .border(Color.gray, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+         }
+}
+
+struct AccountV: View {
+    @State private var inputText: String = ""
+
+    @State var collection1 : [Card] = [
+        Card(id: 0, title: "Your Profile", imageUrl: "user"),
+        Card(id: 1, title: "Your Orders", imageUrl: "shopping"),
+        Card(id: 2, title: "Your Address", imageUrl: "pin"),
+        Card(id: 3, title: "Your Reviews", imageUrl: "favourite"),
+        Card(id: 4, title: "Support", imageUrl: "customer_service"),
+        Card(id: 5, title: "Share App", imageUrl: "share"),
+        Card(id: 6, title: "Rate Us", imageUrl: "star"),
+        Card(id: 7, title: "Wishlist", imageUrl: "heart"),
+        Card(id: 8, title: "Logout", imageUrl: "arrow_exit")
+    ]
+    
+    var body: some View {
+        VStack{
+            NavigationHeader()
+                .searchable(text: $inputText)
+                .background(Color.themeColor.ignoresSafeArea())
+                .padding(.bottom)
+            Spacer()
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVGrid(columns: [
+                    GridItem(.flexible(), alignment: .top),
+                    GridItem(.flexible(), alignment: .top)
+                ]) {
+                    ForEach(collection1) { collection in
+                        HeaderTab(collection: collection)
+                            
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
     }
 }
 
--- FikFis/Utilities/Extension.swift
+++ FikFis/Utilities/Extension.swift
@@ -237,3 +237,9 @@ extension View {
         self.modifier(CardViewModifier())
     }
 }
+
+extension UIScreen{
+   static let screenWidth = UIScreen.main.bounds.size.width
+   static let screenHeight = UIScreen.main.bounds.size.height
+   static let screenSize = UIScreen.main.bounds.size
+}
